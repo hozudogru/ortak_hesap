022ba41fe84bd8988a14f71dfe5cba19c61921d2
Hasim Ozudogru <hozudogru@gmail.com>
2026-05-09 19:30:34 +0300

    Enable automatic signing for iOS

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ortak_hesap"
+    namespace = "com.hasimozudogru.ortak_hesap"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ortak_hesap"
+        applicationId = "com.hasimozudogru.ortak_hesap"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
